0ee864fc47e728fc13d7e0bb6d8904596b487bd1
Colin S <colin6442@gmail.com>
2022-04-08 22:54:34 -0700

    finished most functionality for calibration setup

--- calibrationPage.js
+++ calibrationPage.js
@@ -1,4 +1,7 @@
 window.saveDataAcrossSessions = true;
+var currentCal = 1;
+var coordsX = ["5", "30", "50", "70", "95"];
+var coordsY = ["10", "37.5", "62.5", "90"];
 
 window.onload = async function() {
 	isCalibrated();
@@ -11,8 +14,11 @@ var beginCalibration = async function() {
     webgazerInstance.showVideoPreview(false) /* shows all video previews */
         .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
         .applyKalmanFilter(true); // Kalman Filter defaults to on.
-        // Add the SVG component on the top of everything.
+        
+    webgazer.removeOnlyMouseMoveListeners();
     webgazer.setGazeListener(eyeListener);
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("calibration").style.display = "block";
 };
 
 window.onbeforeunload = function() {
@@ -36,13 +42,47 @@ function isCalibrated(){
 	if(data != null){
 		console.log("Has data");
 		document.getElementById("calibratedText").style.display = "block";
+		document.getElementById("checkBtn").style.display = "block";
+		document.getElementById("contBtn").style.display = "block";
+		document.getElementById("resetBtn").style.display = "block";
 	}else{
 		console.log("No data");
+		document.getElementById("calBtn").style.display = "block";
 	}
 	});
 }
 
 function resetCalibration(){
-    localforage.clear();
+  localforage.clear();
 	location.reload();
+}
+
+var checkCalibration = async function() {
+  // document.getElementById("calibration").style.display = "block";
+  // document.getElementById("menu").style.display = "none";
+  const webgazerInstance = await webgazer.setRegression('weightedRidge') /* currently must set regression and tracker */
+    .setTracker('TFFacemesh')
+    .begin();
+  webgazerInstance.showVideoPreview(false) /* shows all video previews */
+    .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
+    .applyKalmanFilter(true); // Kalman Filter defaults to on.
+  webgazer.removeOnlyMouseMoveListeners();
+  webgazer.setGazeListener(eyeListener);
+}
+
+function clickedBtn(){
+  if(currentCal < 21){
+    var x, y;
+    x = currentCal % 5;
+    y = Math.floor(currentCal/5);
+    document.getElementById("calibrationBtn").style.marginTop = "calc(" + coordsY[y] + "vh - 25px)";
+    document.getElementById("calibrationBtn").style.marginLeft = "calc(" + coordsX[x] + "vw - 25px)";
+    currentCal++;
+  }else{
+    location.href = "./readingPage.html";
+  }
+}
+
+function finishCalibration(){
+  location.href = "./readingPage.html";
 }
