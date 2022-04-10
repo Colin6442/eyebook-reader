f41457e85759185532e69fb975396beab4d3447c
Ahmad Suleiman <60371882+MadSuleiman@users.noreply.github.com>
2022-04-10 14:03:24 -0700

    Button functionality
    PDF is passed in, haven't attempted to access it however

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('startPage.html')
 
+@app.route('/reading', methods=['GET'])
+def reading():
+    return render_template('readingPage.html')
+
 if __name__ == "__main__":
     app.run()
--- static/js/startPage.js
+++ static/js/startPage.js
@@ -1,1 +1,4 @@
 //This is a comment for this page aswell
+document.getElementById("pdfFile").onchange = function() {
+    document.getElementById("form").submit();
+};
