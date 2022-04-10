b33f5f13f51d3e286a0c860c0ea572547fd9346d
Ahmad Suleiman <60371882+MadSuleiman@users.noreply.github.com>
2022-04-10 14:19:56 -0700

    Updated most files to match Main's

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from flask import render_template, redirect, request, url_for
 from flask import Flask
 
 app = Flask(__name__)
-# Our main page, which renders the full ucm map
+
 @app.route('/', methods=['GET', 'POST'])
+def start():
+    return redirect(url_for("home"))
+
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('startPage.html')
 
