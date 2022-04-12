5e12525e2547f989f14947994ebe3096140a36a1
Ahmad Suleiman <60371882+MadSuleiman@users.noreply.github.com>
2022-04-12 00:00:44 -0700

    No longer borrowing code

--- static/js/readingPage.js
+++ static/js/readingPage.js
@@ -1,4 +1,3 @@
-// Borrowed from https://forums.tumult.com/t/replace-inner-html-with-text-from-an-external-file/2129/3
 fetch('../../uploads/upload.txt')
   .then(response => response.text())
   .then(text => document.getElementById("txtSect").innerHTML = text)
